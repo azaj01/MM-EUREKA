6cf2b523f3811f13dc1f0ceac66934b0532b74eb
leodu <lxdu0314@gmail.com>
2025-03-11 16:34:00 +0800

    fix

--- openrlhf/trainer/ppo_utils/__init__.py
+++ openrlhf/trainer/ppo_utils/__init__.py
@@ -1,3 +1,4 @@
+from .data_processor import DATA_PROCESSOR_MAP
 from .experience_maker import Experience, NaiveExperienceMaker, RemoteExperienceMaker
 from .kl_controller import AdaptiveKLController, FixedKLController
 from .replay_buffer import NaiveReplayBuffer
@@ -9,4 +10,5 @@ __all__ = [
     "AdaptiveKLController",
     "FixedKLController",
     "NaiveReplayBuffer",
+    "DATA_PROCESSOR_MAP",
 ]
